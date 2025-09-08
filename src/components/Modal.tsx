349a0ff9a87d15e9d1801696f3397d7f10e12b53
Your Name <Siriwipha_a@cmu.ac.th>
2025-09-08 11:43:32 +0700

    fn

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,37 +1,37 @@
-import { useState } from "react";
-import {
-  Modal,
-  TextInput,
-  NumberInput,
-  Select,
-  Button,
-  Stack,
-} from "@mantine/core";
+//import { useState } from "react";
+//import {
+//  Modal,
+ // TextInput,
+ // NumberInput,
+//  Select,
+//  Button,
+//  Stack,
+//} from "@mantine/core";
 
-type AddFoodModalProps = {
-  opened: boolean;
-  onClose: () => void;
-  onAdd: (
-    name: string,
-    price: number | string,
-    quantity: number | string,
-    category: string
-  ) => void;
-};
+//type AddFoodModalProps = {
+  //opened: boolean;
+// onClose: () => void;
+  //onAdd: (
+   // name: string,
+   // price: number | string,
+   // quantity: number | string,
+   // category: string
+  //)// => void;
+//};
 
-export default function AddFoodModal({}: AddFoodModalProps) {
-  const [name, setName] = useState<string>("");
-  const [price, setPrice] = useState<number | string>(0);
-  const [quantity, setQuantity] = useState<number | string>(0);
-  const [category, setCategory] = useState<string | null>(null);
+//export default function AddFoodModal({}: AddFoodModalProps) {
+  //const [name, setName] = useState<string>("");
+// const [price, setPrice] = useState<number | string>(0);
+// const [quantity, setQuantity] = useState<number | string>(0);
+//  const [category, setCategory] = useState<string | null>(null);
 
-  const handleSubmit = () => {};
+//  const handleSubmit = () => {};
 
   // หากต้องการแปง type string เป็น type number สามารถดูตัวอย่างนี้ได้
   let val_number: number = Number("500.0");
   console.log(val_number + 100); // 600.0
 
-  return {
+//  return {
     /* Type additional text here. */
-  };
-}
+//  };
+//}
